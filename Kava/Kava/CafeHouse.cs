adcd03fabc897525230d0dc2e7a482265d8eebf9
leg <legweb33@gmail.com>
2016-05-09 02:13:23 +0300

    first comit

---
+++ Kava/Kava/CafeHouse.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Threading;
+namespace Kava
+{
+    public class CafeHouse:INotifyPropertyChanged
+    {
+        private Customer newCustumer;
+        private Queue _queue;
+        private Tables _tables;
+        private ListBox _listBoxTable;
+        private ListBox _listBoxQueue;
+        public DispatcherTimer _dispatcherAddQueue;
+        public DispatcherTimer _dispatcherRemoveTable;
+        private TimeSpan QTCompere = new TimeSpan(0, 0, 0);
+        public Queue Queue
+        {
+            get { return _queue; }
+            set 
+            {
+                _queue = value;
+                Notify("Queue");
+            }
+        }
+        public Tables Tables
+        {
+            get { return _tables;}
+            set 
+            {
+                _tables = value;
+                Notify("Tables");
+            }
+        }
+        /*public DispatcherTimer Dispatcher
+        {
+            get { return _dispatcher;}
+            set
+            {
+                _dispatcher = value;
+                Notify("Dispatcher");
+            }
+        }
+         * */
+        public CafeHouse(ListBox queue, ListBox tables)
+        {
+            this._queue = new Queue();
+            this._tables = new Tables();
+            this._dispatcherAddQueue = new DispatcherTimer(){Interval = TimeSpan.FromSeconds(1)};
+            this._dispatcherAddQueue.Tick += DispatherAddQueueTick;
+
+            this._dispatcherRemoveTable = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
+            this._dispatcherRemoveTable.Tick += DispatherRemoveTableTick;
+
+            _listBoxQueue = queue;
+            _listBoxTable = tables;        
+            _listBoxQueue.ItemsSource = this._queue.Collection;
+            _listBoxTable.ItemsSource = this._tables.Collection;
+
+            
+        }
+        private void DispatherAddQueueTick(object sender,EventArgs e) 
+        {
+            CreateCuctumer();
+            
+           
+        }
+        private void DispatherRemoveTableTick(object sender, EventArgs e)
+        {
+           
+            GoOutTable();
+
+        }
+        public void CreateTables(int countTables)
+        {
+            for(int i = 0; i < countTables; i++) //paralel.for
+            {
+                Tables.Add(new Customer());
+            }
+        }
+        public void CreateCuctumer()//create custumer in queue(check when we must create new custimer)
+        {
+            if (newCustumer == null)
+            {
+                newCustumer = new Customer();
+                QTCompere = new TimeSpan(0, 0, 0);
+            }
+            else
+            {
+                QTCompere += TimeSpan.FromSeconds(1);
+                if (QTCompere == newCustumer._timeQueue)
+                {
+                    Queue.Add(newCustumer);
+                    newCustumer = null;
+                }
+            }
+            
+        }
+        public event PropertyChangedEventHandler  PropertyChanged;
+
+        private void Notify(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                 PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+        private void GoOutTable()//add 1 second to time customer and chek time for siting on tables
+        {
+            for (int i=0; i < 5;i++)
+            {
+                var customer = Tables[i];
+                customer.Time += TimeSpan.FromSeconds(1);
+                if (customer.Time == customer.TimeTable) //переробити в нестатичну таблес
+                {
+                    Tables.GoOut(Tables, customer);
+                    customer.Time = new TimeSpan(0, 0, 0);
+                }
+
+            }
+            
+            
+        }
+    }
+}
+
---
+++ Kava/Kava/MyCollection.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Kava
+{
+    public class MyCollection : INotifyPropertyChanged
+    {
+
+        protected ObservableCollection<Customer> _collection;
+        protected int _count;
+
+        public ObservableCollection<Customer> Collection
+        {
+            get { return _collection; }
+            set
+            {
+                _collection = value;
+                Notify("Collection");
+            }
+        }
+        public int Cont
+        {
+            get { return _count; }
+            set { _count = value; }
+        }
+        public MyCollection()
+        {
+            this._collection = new ObservableCollection<Customer>();
+
+        }
+        public void Add(Customer custumer)
+        {
+            Collection.Add(custumer);
+        }
+        public Customer this[int i]
+        {
+            set
+            {
+                _collection[i] = value;
+            }
+            get
+            {
+                return _collection[i];
+            }
+        }
+        public void Remove(Customer customer)
+        {
+            foreach (Customer c in Collection)
+            {
+                if (c == customer)
+                {
+                    Collection.Remove(customer);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Notify(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+        static public void GoOut(MyCollection collection, Customer customer)//null obj customer in table
+        {
+            
+            var newCustomer = new Customer(false)
+                {
+                    Name = "Empty Table",
+                    Progress = 0,
+                    Time = new TimeSpan(0, 0, 0),
+                    TimeQueue = new TimeSpan(0, 0, 0),
+                    TimeTable = new TimeSpan(0, 0, 0)
+
+                };
+            for (int i = 0; i < 5; i++)
+            {
+                if (customer.Equals(collection[i]))
+
+                    collection[i] = newCustomer;
+
+            }
+        }
+
+
+    }
+
+}
+
